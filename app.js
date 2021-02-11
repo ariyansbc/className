424d85a4a1ff9be4ed648a63f0fd1a6c1664be1b
shahriar jalal <shahjalalsbc@gmail.com>
2021-02-11 12:32:39 +0600

    js className property

---
+++ app.js
@@ -0,0 +1,11 @@
+
+const Pname = document.getElementById("Pname");
+const boxId = document.getElementById("box")
+
+
+const doCss = () => {
+    Pname.className = "nameClass";
+    boxId.className = "boxClass"
+    document.body.style.background = "#000"
+    document.body.style.color = "#fff"
+}
